e4e43fe977ac91561896b81dd48380cb468e6930
winlin <winterserver@126.com>
2013-09-28 23:44:57 +0800

    add rtmp start script

---
+++ start_rtmp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cpu_id=`ps aux|grep st|grep http|grep load|wc -l`
+
+./objs/st_rtmp_load --thread=3000 --url=rtmp://127.0.0.1:1935/live/livestream id=${cpu_id} 1>/dev/null 2>>report.log &
+ret=$?; if [[ 0 -ne ${ret} ]];then echo "start process failed, ret=${ret}"; exit ${ret}; fi
+
+pid=`ps aux|grep st|grep http|grep load|grep id=${cpu_id}|awk '{print $2}'`
+ret=$?; if [[ 0 -ne ${ret} ]];then echo "get process pid failed, ret=${ret}"; exit ${ret}; fi
+
+echo "pid=${pid} cpu_id=$cpu_id"
+
+sudo taskset -pc ${cpu_id} ${pid}
+ret=$?; if [[ 0 -ne ${ret} ]];then echo "bind cpu failed, ret=${ret}"; exit ${ret}; fi
+
+echo "$pid started bind to ${cpu_id}"

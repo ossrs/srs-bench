1a6de6ada0699690547338df32bc3a87ea47ab6b
winlin <winlin@vip.126.com>
2014-12-02 14:32:26 +0800

    add MIT license.

--- src/core/htl_core_error.hpp
+++ src/core/htl_core_error.hpp
@@ -1,3 +1,26 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013-2014 winlin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #ifndef _htl_core_error_hpp
 #define _htl_core_error_hpp
 
@@ -33,7 +56,7 @@
 #define ERROR_RTMP_MSG_TOO_BIG 602
 #define ERROR_RTMP_INVALID_RESPONSE 603
 
-#define ProductVersion "1.0.3"
+#define ProductVersion "1.0.4"
 #define ProductHTTPName "Bravo HttpLoad/"ProductVersion
 #define ProductHLSName "Bravo HlsLoad/"ProductVersion
 #define ProductRtmpName "Bravo RtmpPlayLoad/"ProductVersion

b612e5195f51a30f30a846ea55494278d95e64a2
winlin <winlin@vip.126.com>
2021-02-01 21:01:09 +0800

    Add depends pion/webrtc

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ossrs/srs-bench
+
+go 1.15
+
+require github.com/pion/webrtc/v3 v3.0.4
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    "github.com/pion/webrtc/v3"
+)
+
+func main() {
+    fmt.Println(webrtc.PeerConnection{})
+}

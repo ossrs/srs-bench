0d06a8c910e0a670531bf81ee252704aae6a2e63
winlin <winlin@vip.126.com>
2021-03-13 07:55:26 +0800

    Sync with pion/transport

--- vnet/udpproxy.go
+++ vnet/udpproxy.go
@@ -1,23 +1,3 @@
-// The MIT License (MIT)
-//
-// Copyright (c) 2021 Winlin
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of
-// this software and associated documentation files (the "Software"), to deal in
-// the Software without restriction, including without limitation the rights to
-// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// the Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 package vnet
 
 import (
@@ -26,7 +6,7 @@ import (
 	"time"
 )
 
-// A UDP proxy between real server(net.UDPConn) and vnet.UDPConn.
+// UDPProxy is a proxy between real server(net.UDPConn) and vnet.UDPConn.
 //
 // High level design:
 //                           ..............................................
@@ -42,29 +22,6 @@ import (
 //                           :            |        |          ............................:
 //                           :            +--------+                       :
 //                           ...............................................
-//
-// The whole big picture:
-//                           ......................................
-//                           :         Virtual Network (vnet)     :
-//                           :                                    :
-//   +-------+ *         1 +----+         +--------+              :
-//   | :App  |------------>|:Net|--o<-----|:Router |          .............................
-//   +-------+             +----+         |        |          :        UDPProxy           :
-//   +-----------+ *     1 +----+         |        |       +----+     +---------+     +---------+     +--------+
-//   |:STUNServer|-------->|:Net|--o<-----|        |--->o--|:Net|-->o-| vnet.   |-->o-|  net.   |--->-| :Real  |
-//   +-----------+         +----+         |        |       +----+     | UDPConn |     | UDPConn |     | Server |
-//   +-----------+ *     1 +----+         |        |          :       +---------+     +---------+     +--------+
-//   |:TURNServer|-------->|:Net|--o<-----|        |          ............................:
-//   +-----------+         +----+ [1]     |        |              :
-//                           :          1 |        | 1  <<has>>   :
-//                           :      +---<>|        |<>----+ [2]   :
-//                           :      |     +--------+      |       :
-//                         To form  |      *|             v 0..1  :
-//                   a subnet tree  |       o [3]      +-----+    :
-//                           :      |       ^          |:NAT |    :
-//                           :      |       |          +-----+    :
-//                           :      +-------+                     :
-//                           ......................................
 type UDPProxy struct {
 	// The router bind to.
 	router *Router
@@ -143,35 +100,8 @@ func (v *aUDPProxyWorker) Proxy(client *Net, serverAddr *net.UDPAddr) error { //
 		return err
 	}
 
-	// Start a proxy goroutine.
-	var findEndpointBy func(addr net.Addr) (*net.UDPConn, error)
-	// nolint:godox // TODO: FIXME: Do cleanup.
-	go func() {
-		buf := make([]byte, 1500)
-
-		for {
-			n, addr, err := vnetSocket.ReadFrom(buf)
-			if err != nil {
-				return
-			}
-
-			if n <= 0 || addr == nil {
-				continue // Drop packet
-			}
-
-			realSocket, err := findEndpointBy(addr)
-			if err != nil {
-				continue // Drop packet.
-			}
-
-			if _, err := realSocket.Write(buf[:n]); err != nil {
-				return
-			}
-		}
-	}()
-
 	// Got new vnet client, start a new endpoint.
-	findEndpointBy = func(addr net.Addr) (*net.UDPConn, error) {
+	findEndpointBy := func(addr net.Addr) (*net.UDPConn, error) {
 		// Exists binding.
 		if value, ok := v.endpoints.Load(addr.String()); ok {
 			// Exists endpoint, reuse it.
@@ -215,6 +145,32 @@ func (v *aUDPProxyWorker) Proxy(client *Net, serverAddr *net.UDPAddr) error { //
 
 		return realSocket, nil
 	}
+
+	// Start a proxy goroutine.
+	// nolint:godox // TODO: FIXME: Do cleanup.
+	go func() {
+		buf := make([]byte, 1500)
+
+		for {
+			n, addr, err := vnetSocket.ReadFrom(buf)
+			if err != nil {
+				return
+			}
+
+			if n <= 0 || addr == nil {
+				continue // Drop packet
+			}
+
+			realSocket, err := findEndpointBy(addr)
+			if err != nil {
+				continue // Drop packet.
+			}
+
+			if _, err := realSocket.Write(buf[:n]); err != nil {
+				return
+			}
+		}
+	}()
 
 	return nil
 }
